c3bd304d260dd94ea09d78eac231834713bbd9c2
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-03-21 06:56:31 -0500

    📈Refactor code to include sensor value and time tracking

--- Experiment/CapacitorPlotting/CapacitorPlotting.ino
+++ Experiment/CapacitorPlotting/CapacitorPlotting.ino
@@ -1,12 +1,28 @@
-int ValorSensor = 0;  
+// Variable para almacenar el valor leído del sensor
+int ValorSensor = 0;
+// Variable para almacenar el tiempo desde que el Arduino empezó a ejecutarse
+unsigned long tiempo;
+
 void setup() 
 {
   Serial.begin(9600);
   while(!Serial) {}
 }
+
 void loop() 
 {
+  // Obtiene el tiempo actual en milisegundos
+  tiempo = millis();
+
+  // Lee el valor del pin A0 y lo almacena en la variable ValorSensor
   ValorSensor = analogRead(A0);
+
+  // Envía el valor leído y el tiempo transcurrido al Monitor Serial
+  // Formato: tiempo,ValorSensor
+  Serial.print(tiempo);
+  Serial.print(",");
   Serial.println(ValorSensor);
+
+  // Espera 100 milisegundos antes de leer el siguiente valor
   delay(100);
 }
